6c2857047cc19b414f822ef6bcef684bfeb231bf
Vivek300705 <vivekkumarsulaniya@gmail.com>
2025-06-01 12:43:16 +0530

    Adds Home page layout and CallToAction component
    Introduces a new Home page layout for students, including structured sections for Hero, Companies, CourseSection, TestimonialSections, CallToAction, and Footer components.
    
    Enhances the CallToAction component with responsive design, buttons for user actions, and descriptive text to encourage engagement. Improves visual hierarchy and alignment for better user experience.

---
+++ .history/client/src/components/student/Footer_20250601120557.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer>
+        <div>
+
+        </div>
+        <p>
+            
+        </p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ .history/client/src/components/student/TestimonialSections_20250601113435.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { dummyTestimonial } from '../../assets/assets'
+
+function TestimonialSections() {
+  return (
+    <div className='pb-14 px-8 md:px-0'>
+    <h2 className='text-3xl  font-medium text-gray-800 '>
+      Testimonials
+    </h2>
+    <p className='text-gray-500 md:text-base mt-3'>
+      Hear from our learners as they share their journey of the transfomation ,success and how our <br/>platform has made a difference in their lives.
+    </p>
+    <div>
+      {dummyTestimonial.map((testimonial, index) => (<div key={index}>
+        <div>
+          <img src={testimonial.image} alt={testimonial.name} />
+          <div >
+            <h1>
+              {testimonial.name}
+            </h1>
+            <p>
+              {testimonial.role}
+              </p>
+          </div>
+          <div>
+            <div>
+              {[...Array(5).map((-React,i))]}
+            </div>
+          </div>
+        </div>
+      </div>))}
+    </div>
+    </div>
+  )
+}
+
+export default TestimonialSections

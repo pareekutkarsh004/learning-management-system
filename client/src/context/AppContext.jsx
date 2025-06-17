ce3de96efa86a2f178862b8b0b024129d25454bc
abhishekdhakad522 <abhishekdhakad522@gmail.com>
2025-06-17 15:50:53 +0530

    Add axios and react-toastify; implement course and user data fetching in AppContext

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -2,42 +2,104 @@ import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
 import {useAuth,useUser} from "@clerk/clerk-react"
 import humanizeDuration from "humanize-duration";
+import axios from 'axios'
+import { toast } from "react-toastify";
 export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
+
+    const backendURL = import.meta.env.VITE_BACKEND_URL;
+
     const currency = import.meta.env.VITE_CURRENCY || "₹"; // fallback to ₹ if undefined
     
     const {getToken} = useAuth()
     const {user} =useUser() 
     const [allCourses, setAllCourses] = useState([]);
-    const [isEducator, setIsEducator] = useState(true);
+    const [isEducator, setIsEducator] = useState(false);
     const [enrolledCourses, setEnrolledCourses] = useState([]);
+    const [userData, setUserData] = useState(null);
 
     // Fetch all courses from the server or use dummy
     const fetchAllCourses = async () => {
-        setAllCourses(dummyCourses);
+        // setAllCourses(dummyCourses);
+        try {
+            const {data} = await axios.get(backendURL + '/api/course/all');
+            if(data.success){
+                setAllCourses(data.courses)
+            }else{
+              toast.error(data.message)
+            }
+        } catch (error) {
+             toast.error(error.message)
+        }
         // In real use-case, you can fetch from API here
         // const response = await axios.get('/api/courses');
         // setAllCourses(response.data);
-    };  //fetch enrolled courses from the server or use dummy
+    };  
+    
+    //Fetch User data
+    const fetchUserData = async()=>{
+
+        if(user?.publicMetadata?.role === 'educator'){
+            setIsEducator(true);
+        }
+
+        try {
+            const token = await getToken();
+
+            const response = await axios.get(backendURL +'/api/user/data',{headers: 
+                {Authorization: `Bearer ${token}`}})
+            
+            if(response.data.success){
+                setUserData(response.data.user)
+            }
+            else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+    
+    //fetch enrolled courses from the server or use dummy
     const fetchUserEnrolledCourses = async () => {
+
         // In real use-case, you can fetch from API here
         // const response = await axios.get('/api/enrolled-courses');
         // setEnrolledCourses(response.data);
-        setEnrolledCourses(dummyCourses); // Using dummy data for now
+        // setEnrolledCourses(dummyCourses); // Using dummy data for now
+
+        try {
+            const token= await getToken();
+            const{data} = await axios.get(backendURL +'/api/user/enrolled-courses',
+                {headers:{Authorization:`Bearer ${token}`}})
+    
+            if(data.success){
+                setEnrolledCourses(data.enrolledCourses.reverse())
+            }
+            else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+
     };
 
     useEffect(() => {
         fetchAllCourses();
-        fetchUserEnrolledCourses();
+        // fetchUserEnrolledCourses();
     }, []);
 
-   const logToken = async()=>{
-    console.log(await getToken());
-   }
+
+//    const logToken = async()=>{
+//     console.log(await getToken());
+//    }
     useEffect(()=>{
        if(user){
-         logToken() 
+        //  logToken() 
+         fetchUserData()
+         fetchUserEnrolledCourses();
        } 
     },[user])
 
@@ -48,7 +110,8 @@ export const AppContextProvider = (props) => {
             (sum, rating) => sum + rating.rating,
             0
         );
-        return (totalRating / course.courseRatings.length).toFixed(1);
+        // return (totalRating / course.courseRatings.length).toFixed(1);
+        return Math.floor (totalRating / course.courseRatings.length)
     };
 
     // ✅ Calculate chapter time
@@ -99,6 +162,11 @@ export const AppContextProvider = (props) => {
         enrolledCourses,
         setEnrolledCourses,
         fetchAllCourses,
+        backendURL,
+        userData,
+        setUserData,
+        getToken,
+        
     };
 
     return (

698bda6b16895c813db2637131f6436e24b8ed7b
abhishekdhakad522 <abhishekdhakad522@gmail.com>
2025-06-18 17:34:02 +0530

    Fetch dashboard data from the backend in Dashboard component; handle success and error responses with toast notifications

--- client/src/components/pages/educator/Dashboard.jsx
+++ client/src/components/pages/educator/Dashboard.jsx
@@ -2,18 +2,36 @@ import React, { useContext, useEffect, useState } from 'react';
 import assets, { dummyDashboardData } from '../../../assets/assets';
 import Loading from '../../student/Loading';
 import { AppContext } from '../../../context/AppContext';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const Dashboard = () => {
-  const { currency } = useContext(AppContext);
+  const { currency,backenUrl, isEducator, getToken } = useContext(AppContext);
   const [dashboardData, setDashboardData] = useState(null);
 
   const fetchDashboardData = async () => {
-    setDashboardData(dummyDashboardData);
+    try {
+      const token = await getToken()
+      const {data}= await axios.get(backenUrl+'/api/educator/dashboard',
+        {headers:{Authorization:`Bearer ${token}`}})
+
+        if(data.success)
+        {
+          setDashboardData(data.dashboardData)
+        }
+        else{
+          toast.error(data.message)
+        }
+    } catch (error) {
+      toast.error(error.message)
+    }
   };
 
   useEffect(() => {
+    if(isEducator){
     fetchDashboardData();
-  }, []);
+    }
+  }, [isEducator]);
 
   return dashboardData ? (
     <div className="min-h-screen flex flex-col gap-8 md:p-8 p-4 pt-8 w-full max-w-6xl mx-auto">
